7714e91ef7422bf417f69ebc1aa3eda00ee52098
Alan Silva <jasr.rabello@gmail.com>
2024-07-25 18:03:44 -0300

    Commit inicial

---
+++ criacao-de-api-dio/src/test/java/desafio/api/criacao_de_api_dio/CriacaoDeApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.api.criacao_de_api_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriacaoDeApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
